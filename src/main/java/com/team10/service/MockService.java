da77c5011886885f469e0da156d3350aa73aec86
tkdgusl94 <tkdgusl94@gmail.com>
2019-07-16 12:31:30 +0900

    create MockService class

---
+++ src/main/java/com/team10/service/MockService.java
@@ -0,0 +1,28 @@
+package com.team10.service;
+import com.team10.domain.Subject;
+import com.team10.repository.MockRepository;
+
+import java.util.List;
+
+public class MockService {
+    private final MockRepository mockRepository;
+
+    public MockService(MockRepository mockRepository) {
+        this.mockRepository = mockRepository;
+    }
+
+    public List<Subject> findAllSubjects() {
+        return mockRepository.findAll();
+    }
+
+    public Subject findSubjectByName(String name) {
+        Subject subject = mockRepository.findSubjectByName(name);
+        return subject;
+    }
+
+    public void addSubject(Subject subject) {
+        Subject addSubject = new Subject(subject.getName(), subject.getCode(), subject.getPrerequsiteSubject(), subject.getGrade());
+        mockRepository.addSubject(addSubject);
+        return;
+    }
+}
