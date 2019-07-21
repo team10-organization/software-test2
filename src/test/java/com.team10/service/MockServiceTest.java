fa655cfe4a4c834dd8825128612e5718cbfc3dec
MaSungHo <tonem123@naver.com>
2019-07-21 13:14:33 +0900

    Add the test method that returns the Subject object when users put the name of subject

--- src/test/java/com.team10/service/MockServiceTest.java
+++ src/test/java/com.team10/service/MockServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 
@@ -82,4 +83,12 @@ public class MockServiceTest {
 
 
     }
+
+    @Test
+    public void 과목명을_입력하면_클래스를_리턴() {
+        Subject subject = mock(Subject.class);
+        when(mockService.findSubjectByName("실전코딩")).thenReturn(new Subject("실전코딩", "X230",
+                "객체지향프로그래밍", 3));
+        assertThat(mockService.findSubjectByName("실전코딩"), instanceOf(Subject.class));
+    } // 과목명으로 검색하면 Subject 객체를 리턴하는지 테스트하는 코드 changed by 마성호
 }
