d8a3b45c09568313e7e80512789e677bd47dbb88
MaSungHo <52434222+MaSungHo@users.noreply.github.com>
2019-07-21 13:25:34 +0900

    새로운 테스트 메소드 3개 추가했습니다. (#5)
    * Add the test method that returns the Subject object when users put the name of subject
    
    * Add the test method that returns the name of prerequisite when users put the name of subject
    
    * Add the test method that returns the name of the related subject when users put the name of prerequisite subject

--- src/test/java/com.team10/service/MockServiceTest.java
+++ src/test/java/com.team10/service/MockServiceTest.java
@@ -2,6 +2,7 @@ package com.team10.service;
 
 import com.team10.domain.Subject;
 import com.team10.repository.MockRepository;
+import org.hamcrest.core.Is;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -9,6 +10,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 
@@ -82,4 +84,33 @@ public class MockServiceTest {
 
 
     }
+
+    @Test
+    public void 과목명을_입력하면_클래스를_리턴() {
+        Subject subject = mock(Subject.class);
+        when(mockService.findSubjectByName("실전코딩")).thenReturn(new Subject("실전코딩", "X230",
+                "객체지향프로그래밍", 3));
+        assertThat(mockService.findSubjectByName("실전코딩"), instanceOf(Subject.class));
+    } // 과목명으로 검색하면 Subject 객체를 리턴하는지 테스트하는 코드 changed by 마성호
+
+    @Test
+    public void 과목명을_입력하면_선수과목을_리턴() {
+        Subject subject = mock(Subject.class);
+        when(mockService.findSubjectByName("실전코딩")).thenReturn(new Subject("실전코딩", "X230",
+                "객체지향프로그래밍", 3));
+        assertThat(mockService.findSubjectByName("실전코딩").getPrerequisiteSubject(), Is.is("객체지향프로그래밍"));
+        mockService.findSubjectByName("실전코딩");
+        verify(mockRepository, times(2)).findSubjectByName(anyString());
+    } /* 과목명으로 검색하면 선수과목명을 리턴하는지 테스트하고
+     findSubjectByName가 2번 호출됐는지 확인하는 코드 changed by 마성호 */
+
+    @Test
+    public void 선수과목명을_입력하면_연계된_과목명_리턴() {
+        Subject subject = mock(Subject.class);
+        when(mockService.findSubjectByName("객체지향프로그래밍")).thenReturn(new Subject("실전코딩", "X230",
+                "객체지향프로그래밍", 3));
+        assertThat(mockService.findSubjectByName("객체지향프로그래밍").getName(), Is.is("실전코딩"));
+        verify(subject, never()).setName(anyString());
+    } /* 선수과목명으로 검색하면 연계된 과목명을 리턴하는지 테스트하고
+     setName 메소드가 호출되지 않았으면 pass하는 코드 changed by 마성호 */
 }
