a77e70ab058793b8469733a992cfdcc29492f6d6
MaSungHo <tonem123@naver.com>
2019-07-21 13:16:38 +0900

    Add the test method that returns the name of prerequisite when users put the name of subject

--- src/test/java/com.team10/service/MockServiceTest.java
+++ src/test/java/com.team10/service/MockServiceTest.java
@@ -2,6 +2,7 @@ package com.team10.service;
 
 import com.team10.domain.Subject;
 import com.team10.repository.MockRepository;
+import org.hamcrest.core.Is;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -91,4 +92,15 @@ public class MockServiceTest {
                 "객체지향프로그래밍", 3));
         assertThat(mockService.findSubjectByName("실전코딩"), instanceOf(Subject.class));
     } // 과목명으로 검색하면 Subject 객체를 리턴하는지 테스트하는 코드 changed by 마성호
+
+    @Test
+    public void 과목명을_입력하면_선수과목을_리턴() {
+        Subject subject = mock(Subject.class);
+        when(mockService.findSubjectByName("실전코딩")).thenReturn(new Subject("실전코딩", "X230",
+                "객체지향프로그래밍", 3));
+        assertThat(mockService.findSubjectByName("실전코딩").getPrerequisiteSubject(), Is.is("객체지향프로그래밍"));
+        mockService.findSubjectByName("실전코딩");
+        verify(mockRepository, times(2)).findSubjectByName(anyString());
+    } /* 과목명으로 검색하면 선수과목명을 리턴하는지 테스트하고
+     findSubjectByName가 2번 호출됐는지 확인하는 코드 changed by 마성호 */
 }
